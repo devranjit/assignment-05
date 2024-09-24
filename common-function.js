01dbfe52c59992c3a97f1f5e9c5fb6f517d663bd
devranjit <dvranjitplays@gmail.com>
2024-09-24 20:03:51 +0600

    basic update for popup

--- common-function.js
+++ common-function.js
@@ -1,5 +1,5 @@
 
- let mainBalance = 0 ;
+let mainBalance = 0 ;
 
 function collectingTotalDonation(inputId, submitId, totalId, balanceId, formId){
     const submit = document.getElementById(submitId);
@@ -23,6 +23,8 @@ function collectingTotalDonation(inputId, submitId, totalId, balanceId, formId){
         if(!isNaN(inputAmount) && inputAmount > 0){
             
            if(inputAmount > mainBalance){
+
+            
             const popupSection = document.getElementById('popup-section');
             popupSection.classList.remove('hidden');
 
@@ -30,6 +32,7 @@ function collectingTotalDonation(inputId, submitId, totalId, balanceId, formId){
 
             popupClose.addEventListener('click', function(){
               popupSection.classList.add('hidden');
+              
             })
 
             
@@ -44,34 +47,43 @@ function collectingTotalDonation(inputId, submitId, totalId, balanceId, formId){
             myBalance.innerText = `${mainBalance} BDT`;
 
             inputValue.value = ''; 
+
+
+
+
+
+
+            // here is the history function 
+
+            const historyContainer = document.getElementById('history-container');
+
+            const historySection = document.createElement('div');
+           
+            historySection.style.marginBottom = "15px";
+            historySection.style.border = "2px solid #D1D5DB";
+            historySection.style.padding = "10px"
+            historySection.style.borderRadius = "10px";
+           
+           
+           const donateAmount = document.createElement('h1');
+           donateAmount.classList.add('text-lg', 'font-lexend', 'font-bold', 'py-2');
+            donateAmount.innerText = `${inputAmount} Taka is Donated for ${formName}`;
+            historySection.appendChild(donateAmount);
+            
+            const donationtime = document.createElement('p');
+            donationtime.classList.add('font-lexend', 'font-light', 'text-sm', 'font-gray', 'py-2');
+            donationtime.innerText = `${new Date().toString()}`;
+ 
+            historySection.appendChild(donationtime);
+   
+ 
+            historyContainer.appendChild(historySection);
           }
 
           
 
 
-           const historyContainer = document.getElementById('history-container');
-
-           const historySection = document.createElement('div');
           
-           historySection.style.marginBottom = "15px";
-           historySection.style.border = "2px solid #D1D5DB";
-           historySection.style.padding = "10px"
-           historySection.style.borderRadius = "10px";
-          
-          
-          const donateAmount = document.createElement('h1');
-          donateAmount.classList.add('text-lg', 'font-lexend', 'font-bold', 'py-2');
-           donateAmount.innerText = `${inputAmount} Taka is Donated for ${formName}`;
-           historySection.appendChild(donateAmount);
-           
-           const donationtime = document.createElement('p');
-           donationtime.classList.add('font-lexend', 'font-light', 'text-sm', 'font-gray', 'py-2');
-           donationtime.innerText = `${new Date().toString()}`;
-
-           historySection.appendChild(donationtime);
-  
-
-           historyContainer.appendChild(historySection);
            
            
           
@@ -85,7 +97,6 @@ function collectingTotalDonation(inputId, submitId, totalId, balanceId, formId){
       })
 
 }
-
 
 
 
